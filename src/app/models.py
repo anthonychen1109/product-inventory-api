4c0495a35dc628a17a3844f81a2e843b7f30ecf3
ac065455 <chen.anthony.1109@gmail.com>
2018-03-22 22:46:30 -0400

    added models

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    price = models.DecimalField(null=False, blank=False)
+    quantity_on_hand = models.PositiveIntegerField()
+
+class Inventory(models.Model):
+    id = models.ForeignKey(Product)
+    sum = models.DecimalField()
